cee2025c0061f149718b140ac8d6a8d5625a5d52
TomDB22 <tomdeboysson@icloud.com>
2025-10-02 13:49:56 +0200

    Exercice 3

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def exercice2():
     prenom=input("Quelle est votre prénom ?")
     print("Votre prénom est ", prenom)
 
+def exercice3():
+    print("1er ligne \n 2eme ligne \n 3eme ligne")
+
 def main():
     # Demande à l'utilisateur quel exercice exécuter
     choix = input("Entrez le numéro de l'exercice à exécuter : ")
@@ -13,6 +16,10 @@ def main():
         exercice1()
     elif choix=="2":
         exercice2()
+    elif choix=="3":
+        exercice3()
+    elif choix=="4":
+        exercice4()
     else:
         print("Exercice non reconnu.")
 if __name__ == "__main__":
